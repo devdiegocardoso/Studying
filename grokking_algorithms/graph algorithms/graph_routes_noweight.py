91bcebc77c4ed970315ff85e828f235ead3ce76e
Diego Cardoso <dev.diegocardoso@outlook.com>
2023-05-16 14:24:09 +0200

    Update: Improvements for better code quality

--- grokking_algorithms/graph algorithms/graph_routes_noweight.py
+++ grokking_algorithms/graph algorithms/graph_routes_noweight.py
@@ -32,15 +32,19 @@ graph = {
 }
 
 def print_route(end, parents_list):
+    print("Route found!")
     route = [end.hash_value()]
     current_node = parents_list[end.hash_value()][0]
+    create_route(parents_list, route, current_node)
+    print(route)
+
+def create_route(parents_list, route, current_node):
     while current_node:
         route.append(current_node[0])
         current_node = parents_list[current_node[0]]
     route.reverse()
-    print(route)
 
-def create_children_list(parents_list, current_parent):
+def create_parents_list(parents_list, current_parent):
     for child in graph[current_parent.hash_value()]:
         parents_list.setdefault(child.hash_value(), []).append(current_parent.hash_value())
 
@@ -50,18 +54,20 @@ def bfs(queue):
         current_place = queue.popleft()
         if current_place not in checked:
             if current_place.is_my_goal("golden gate"):
-                print("Route found!")
                 print_route(current_place, parents)
                 return True
-            queue.extend(graph[current_place.hash_value()])
-            checked.append(current_place)
-            create_children_list(parents, current_place)
+            process_node(queue, checked, current_place)
     print("No route found :(")
     return False
+
+def process_node(queue, checked, current_place):
+    queue.extend(graph[current_place.hash_value()])
+    checked.append(current_place)
+    create_parents_list(parents, current_place)
 
 search_queue = deque()
 search_queue.extend(graph[twin_peaks.hash_value()])
 parents = defaultdict(list)
-create_children_list(parents, twin_peaks)
+create_parents_list(parents, twin_peaks)
 
 bfs(search_queue)
