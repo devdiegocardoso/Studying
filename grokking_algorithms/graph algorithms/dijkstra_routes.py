b0c99070d48aab154b3bc230695197cf70709f1d
Diego Cardoso <dev.diegocardoso@outlook.com>
2023-05-23 16:35:32 +0200

    Add: added dijkstra algorithm based on the example illustrated in the book grokking_algorithms

---
+++ grokking_algorithms/graph algorithms/dijkstra_routes.py
@@ -0,0 +1,58 @@
+# pylint: disable=missing-docstring
+
+GOAL = "finish"
+INFINITY = float("inf")
+
+graph = {
+    "start": {"a":6,"b":2},
+    "a": {"finish":1},
+    "b": {"a":3,"finish":5},
+    "finish": {},
+}
+
+costs = {
+    "a":6,
+    "b":2,
+    "finish": INFINITY
+}
+
+parents = {
+    "start": None,
+    "a":"start",
+    "b":"start",
+    "finish": None
+}
+
+processed = []
+
+def find_lowest_cost_node():
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node,cost in costs.items():
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+current_node = find_lowest_cost_node()
+while current_node is not None:
+    cost = costs[current_node]
+    neighbors = graph[current_node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = current_node
+    processed.append(current_node)
+    current_node = find_lowest_cost_node()
+
+track_node = parents[GOAL]
+route = [GOAL]
+while track_node:
+    route.append(track_node)
+    track_node = parents[track_node]
+
+route.reverse()
+print(f"Route: {route}")
+print(f"Minimum time: {costs[GOAL]}")
